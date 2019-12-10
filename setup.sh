2307e41d48e9dcb76e55027b9554302a965fe8c0
Luiz Paulo <mcluiz@gmail.com>
2019-12-10 20:59:44 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
+#packages
 sudo apt install openssh-server net-tools htop tmux
 
+#docker
 curl https://get.docker.com | sh
 sudo usermod -aG docker $USER
+
+#rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source $HOME/.cargo/env
