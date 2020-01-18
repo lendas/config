339be1e24a27c7199d60c6e5b30bb2b71335e510
Luiz Paulo <mcluiz@gmail.com>
2020-01-18 00:06:35 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,3 @@
-#packages
-sudo apt install openssh-server net-tools htop tmux git subversion pv npm
-
-#docker
-curl https://get.docker.com | sh
-sudo usermod -aG docker $USER
-
 #rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
