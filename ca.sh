a834078996027c46bc706e3e293a3c1acc8a6da6
Luiz Paulo <mcluiz@gmail.com>
2020-01-26 07:05:06 -0300

    Update ca.sh

--- ca.sh
+++ ca.sh
@@ -2,20 +2,17 @@ openssl genrsa -des3 -out CA.key 2048
 openssl req -x509 -new -nodes -key CA.key -sha256 -days 1825 -out CA.pem
 openssl genrsa -out toq.key 2048
 openssl req -new -key toq.key -out toq.csr
-cat << EOF > toq.ext
+
+cat << EOF > local.ext
 authorityKeyIdentifier=keyid,issuer
 basicConstraints=CA:FALSE
 keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
 subjectAltName = @alt_names
 
 [alt_names]
-DNS.1 = exemplo
+DNS.1 = localhost
 DNS.2 = local
-DNS.3 = localhost
-DNS.4 = portainer
-DNS.5 = adminer
-DNS.6 = texte.exemplo
 EOF
-openssl x509 -req -in toq.csr -CA CA.pem -CAkey CA.key -CAcreateserial \
--out toq.crt -days 1825 -sha256 -extfile toq.ext
 
+openssl x509 -req -in toq.csr -CA CA.pem -CAkey CA.key -CAcreateserial \
+-out local.crt -days 1825 -sha256 -extfile local.ext
